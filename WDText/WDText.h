be79f3e19538334ba99155bedc4dcca94a449813
王修斌 <wangxiubin@bilibili.com>
2019-05-20 20:17:52 +0800

    Initial Commit

---
+++ WDText/WDText.h
@@ -0,0 +1,13 @@
+//
+//  WDText.h
+//  WDText
+//
+//  Created by wxiubin on 2019/5/20.
+//  Copyright © 2019 wxiubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WDText : NSObject
+
+@end
